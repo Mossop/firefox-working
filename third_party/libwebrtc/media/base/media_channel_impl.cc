dd9ce25f38fc66fd8c5032f0bb1f657430bd0d55
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2026-09-05 14:20:03 +0000

    Bug 2063078 - Cherry-pick libwebrtc v117 415e30fdbb::70aa7e99e4 r=mjf a=dmeehan
    Condensation-Plan-SHA256: db8dbfa3efc03ce16c353834e83d650c1ad32db50d1e8178fbefc4d217fd9676
    Detailed-Commit-Count: 153
    Detailed-Endpoint-Tree: a6235977abcd07111f23fc7ba638c4bcdf55ba8c
    Detailed-Range-First: 61883fb0fa45a95f75e0854aceff3e5805ef2536
    Detailed-Range-Last: 44f4fcdf029d28d517fa61db3f232291a828ad97
    Detailed-Stack-Head: 44f4fcdf029d28d517fa61db3f232291a828ad97
    Source-Orders-First: 71
    Source-Orders-Last: 223
    Upstream-Range-First: 415e30fdbb
    Upstream-Range-Last: 70aa7e99e4
    
    Differential Revision: https://phabricator.services.mozilla.com/D321633

--- third_party/libwebrtc/media/base/media_channel_impl.cc
+++ third_party/libwebrtc/media/base/media_channel_impl.cc
@@ -154,20 +154,20 @@ VideoMediaReceiveInfo::~VideoMediaReceiveInfo() = default;
 VoiceMediaReceiveInfo::VoiceMediaReceiveInfo() = default;
 VoiceMediaReceiveInfo::~VoiceMediaReceiveInfo() = default;
 
-AudioSendParameters::AudioSendParameters() = default;
-AudioSendParameters::~AudioSendParameters() = default;
+AudioSenderParameter::AudioSenderParameter() = default;
+AudioSenderParameter::~AudioSenderParameter() = default;
 
-std::map<std::string, std::string> AudioSendParameters::ToStringMap() const {
-  auto params = RtpSendParameters<AudioCodec>::ToStringMap();
+std::map<std::string, std::string> AudioSenderParameter::ToStringMap() const {
+  auto params = SenderParameters::ToStringMap();
   params["options"] = options.ToString();
   return params;
 }
 
-VideoSendParameters::VideoSendParameters() = default;
-VideoSendParameters::~VideoSendParameters() = default;
+VideoSenderParameters::VideoSenderParameters() = default;
+VideoSenderParameters::~VideoSenderParameters() = default;
 
-std::map<std::string, std::string> VideoSendParameters::ToStringMap() const {
-  auto params = RtpSendParameters<VideoCodec>::ToStringMap();
+std::map<std::string, std::string> VideoSenderParameters::ToStringMap() const {
+  auto params = SenderParameters::ToStringMap();
   params["conference_mode"] = (conference_mode ? "yes" : "no");
   return params;
 }
@@ -188,8 +188,13 @@ MediaChannelUtil::TransportForMediaChannels::~TransportForMediaChannels() {
 
 bool MediaChannelUtil::TransportForMediaChannels::SendRtcp(const uint8_t* data,
                                                            size_t len) {
+  return SendRtcp(rtc::MakeArrayView(data, len));
+}
+
+bool MediaChannelUtil::TransportForMediaChannels::SendRtcp(
+    rtc::ArrayView<const uint8_t> packet) {
   auto send = [this, packet = rtc::CopyOnWriteBuffer(
-                         data, len, kMaxRtpPacketLen)]() mutable {
+                         packet, kMaxRtpPacketLen)]() mutable {
     rtc::PacketOptions rtc_options;
     if (DscpEnabled()) {
       rtc_options.dscp = PreferredDscp();
@@ -209,13 +214,19 @@ bool MediaChannelUtil::TransportForMediaChannels::SendRtp(
     const uint8_t* data,
     size_t len,
     const webrtc::PacketOptions& options) {
+  return SendRtp(rtc::ArrayView<const uint8_t>(data, len), options);
+}
+
+bool MediaChannelUtil::TransportForMediaChannels::SendRtp(
+    rtc::ArrayView<const uint8_t> packet,
+    const webrtc::PacketOptions& options) {
   auto send =
       [this, packet_id = options.packet_id,
        included_in_feedback = options.included_in_feedback,
        included_in_allocation = options.included_in_allocation,
        batchable = options.batchable,
        last_packet_in_batch = options.last_packet_in_batch,
-       packet = rtc::CopyOnWriteBuffer(data, len, kMaxRtpPacketLen)]() mutable {
+       packet = rtc::CopyOnWriteBuffer(packet, kMaxRtpPacketLen)]() mutable {
         rtc::PacketOptions rtc_options;
         rtc_options.packet_id = packet_id;
         if (DscpEnabled()) {
