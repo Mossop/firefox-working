ac9568f101939725f0944560cfb787350f5d9a97
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2026-09-05 14:20:02 +0000

    Bug 2063078 - Cherry-pick libwebrtc v117 c4e0254909::9948623027 r=mjf a=dmeehan
    Condensation-Plan-SHA256: db8dbfa3efc03ce16c353834e83d650c1ad32db50d1e8178fbefc4d217fd9676
    Detailed-Commit-Count: 70
    Detailed-Endpoint-Tree: bb3d69d54d32fa23cc4a0d0915e233a46a375de3
    Detailed-Range-First: 9d17cf17b52e994487856ca386e5d55a3b8af23b
    Detailed-Range-Last: d7569a6304a18a2615c76bee7f74715fb2a1a33f
    Detailed-Stack-Head: 44f4fcdf029d28d517fa61db3f232291a828ad97
    Source-Orders-First: 1
    Source-Orders-Last: 70
    Upstream-Range-First: c4e0254909
    Upstream-Range-Last: 9948623027
    
    Differential Revision: https://phabricator.services.mozilla.com/D321631

--- third_party/libwebrtc/media/engine/webrtc_video_engine.cc
+++ third_party/libwebrtc/media/engine/webrtc_video_engine.cc
@@ -51,6 +51,7 @@
 #include "media/base/rid_description.h"
 #include "media/base/rtp_utils.h"
 #include "media/engine/webrtc_media_engine.h"
+#include "modules/rtp_rtcp/include/receive_statistics.h"
 #include "modules/rtp_rtcp/include/report_block_data.h"
 #include "modules/rtp_rtcp/include/rtcp_statistics.h"
 #include "modules/rtp_rtcp/include/rtp_rtcp_defines.h"
@@ -192,13 +193,9 @@ std::vector<VideoCodec> GetPayloadTypesAndDefaultCodecs(
   supported_formats.push_back(webrtc::SdpVideoFormat(kRedCodecName));
   supported_formats.push_back(webrtc::SdpVideoFormat(kUlpfecCodecName));
 
-  // flexfec-03 is supported as
-  // - receive codec unless WebRTC-FlexFEC-03-Advertised is disabled
-  // - send codec if WebRTC-FlexFEC-03-Advertised is enabled
-  if ((is_decoder_factory &&
-       !IsDisabled(trials, "WebRTC-FlexFEC-03-Advertised")) ||
-      (!is_decoder_factory &&
-       IsEnabled(trials, "WebRTC-FlexFEC-03-Advertised"))) {
+  // flexfec-03 is always supported as receive codec and as send codec
+  // only if WebRTC-FlexFEC-03-Advertised is enabled
+  if (is_decoder_factory || IsEnabled(trials, "WebRTC-FlexFEC-03-Advertised")) {
     webrtc::SdpVideoFormat flexfec_format(kFlexfecCodecName);
     // This value is currently arbitrarily set to 10 seconds. (The unit
     // is microseconds.) This parameter MUST be present in the SDP, but
@@ -310,30 +307,37 @@ static bool ValidateStreamParams(const StreamParams& sp) {
 
   std::vector<uint32_t> primary_ssrcs;
   sp.GetPrimarySsrcs(&primary_ssrcs);
-  std::vector<uint32_t> rtx_ssrcs;
-  sp.GetFidSsrcs(primary_ssrcs, &rtx_ssrcs);
-  for (uint32_t rtx_ssrc : rtx_ssrcs) {
-    bool rtx_ssrc_present = false;
-    for (uint32_t sp_ssrc : sp.ssrcs) {
-      if (sp_ssrc == rtx_ssrc) {
-        rtx_ssrc_present = true;
-        break;
+  for (const auto& semantic :
+       {kFidSsrcGroupSemantics, kFecFrSsrcGroupSemantics}) {
+    if (!sp.has_ssrc_group(semantic)) {
+      continue;
+    }
+    std::vector<uint32_t> secondary_ssrcs;
+    sp.GetSecondarySsrcs(semantic, primary_ssrcs, &secondary_ssrcs);
+    for (uint32_t secondary_ssrc : secondary_ssrcs) {
+      bool secondary_ssrc_present = false;
+      for (uint32_t sp_ssrc : sp.ssrcs) {
+        if (sp_ssrc == secondary_ssrc) {
+          secondary_ssrc_present = true;
+          break;
+        }
+      }
+      if (!secondary_ssrc_present) {
+        RTC_LOG(LS_ERROR) << "SSRC '" << secondary_ssrc
+                          << "' missing from StreamParams ssrcs with semantics "
+                          << semantic << ": " << sp.ToString();
+        return false;
       }
     }
-    if (!rtx_ssrc_present) {
-      RTC_LOG(LS_ERROR) << "RTX SSRC '" << rtx_ssrc
-                        << "' missing from StreamParams ssrcs: "
-                        << sp.ToString();
+    if (!secondary_ssrcs.empty() &&
+        primary_ssrcs.size() != secondary_ssrcs.size()) {
+      RTC_LOG(LS_ERROR)
+          << semantic
+          << " secondary SSRCs exist, but don't cover all SSRCs (unsupported): "
+          << sp.ToString();
       return false;
     }
   }
-  if (!rtx_ssrcs.empty() && primary_ssrcs.size() != rtx_ssrcs.size()) {
-    RTC_LOG(LS_ERROR)
-        << "RTX SSRCs exist, but don't cover all SSRCs (unsupported): "
-        << sp.ToString();
-    return false;
-  }
-
   return true;
 }
 
@@ -752,29 +756,6 @@ WebRtcVideoEngine::CreateReceiveChannel(
     const webrtc::CryptoOptions& crypto_options) {
   return std::make_unique<WebRtcVideoReceiveChannel>(
       call, config, options, crypto_options, decoder_factory_.get());
-}
-
-VideoMediaChannel* WebRtcVideoEngine::CreateMediaChannel(
-    MediaChannel::Role role,
-    webrtc::Call* call,
-    const MediaConfig& config,
-    const VideoOptions& options,
-    const webrtc::CryptoOptions& crypto_options,
-    webrtc::VideoBitrateAllocatorFactory* video_bitrate_allocator_factory) {
-  RTC_LOG(LS_INFO) << "CreateMediaChannel. Options: " << options.ToString();
-  std::unique_ptr<VideoMediaSendChannelInterface> send_channel;
-  std::unique_ptr<VideoMediaReceiveChannelInterface> receive_channel;
-  if (role == MediaChannel::Role::kSend || role == MediaChannel::Role::kBoth) {
-    send_channel = CreateSendChannel(call, config, options, crypto_options,
-                                     video_bitrate_allocator_factory);
-  }
-  if (role == MediaChannel::Role::kReceive ||
-      role == MediaChannel::Role::kBoth) {
-    receive_channel =
-        CreateReceiveChannel(call, config, options, crypto_options);
-  }
-  return new VideoMediaShimChannel(std::move(send_channel),
-                                   std::move(receive_channel));
 }
 
 std::vector<VideoCodec> WebRtcVideoEngine::send_codecs(bool include_rtx) const {
@@ -1300,7 +1281,7 @@ webrtc::RTCError WebRtcVideoSendChannel::SetRtpSendParameters(
 
   return it->second->SetRtpParameters(parameters, std::move(callback));
 }
-absl::optional<VideoCodec> WebRtcVideoSendChannel::GetSendCodec() {
+absl::optional<Codec> WebRtcVideoSendChannel::GetSendCodec() const {
   RTC_DCHECK_RUN_ON(&thread_checker_);
   if (!send_codec()) {
     RTC_LOG(LS_VERBOSE) << "GetSendCodec: No send codec set.";
@@ -1370,7 +1351,7 @@ bool WebRtcVideoSendChannel::AddSendStream(const StreamParams& sp) {
   for (uint32_t used_ssrc : sp.ssrcs)
     send_ssrcs_.insert(used_ssrc);
 
-  webrtc::VideoSendStream::Config config(this);
+  webrtc::VideoSendStream::Config config(transport());
 
   for (const RidDescription& rid : sp.rids()) {
     config.rtp.rids.push_back(rid.rid);
@@ -1600,18 +1581,6 @@ void WebRtcVideoSendChannel::SetVideoCodecSwitchingEnabled(bool enabled) {
   if (allow_codec_switching_) {
     RTC_LOG(LS_INFO) << "Encoder switching enabled.";
   }
-}
-
-bool WebRtcVideoSendChannel::SendRtp(const uint8_t* data,
-                                     size_t len,
-                                     const webrtc::PacketOptions& options) {
-  MediaChannelUtil::SendRtp(data, len, options);
-  return true;
-}
-
-bool WebRtcVideoSendChannel::SendRtcp(const uint8_t* data, size_t len) {
-  MediaChannelUtil::SendRtcp(data, len);
-  return true;
 }
 
 WebRtcVideoSendChannel::WebRtcVideoSendStream::VideoSendStreamParameters::
@@ -2797,8 +2766,9 @@ bool WebRtcVideoReceiveChannel::AddRecvStream(const StreamParams& sp,
   for (uint32_t used_ssrc : sp.ssrcs)
     receive_ssrcs_.insert(used_ssrc);
 
-  webrtc::VideoReceiveStreamInterface::Config config(this, decoder_factory_);
-  webrtc::FlexfecReceiveStream::Config flexfec_config(this);
+  webrtc::VideoReceiveStreamInterface::Config config(transport(),
+                                                     decoder_factory_);
+  webrtc::FlexfecReceiveStream::Config flexfec_config(transport());
   ConfigureReceiverRtp(&config, &flexfec_config, sp);
 
   config.crypto_options = crypto_options_;
@@ -3650,6 +3620,29 @@ WebRtcVideoReceiveChannel::WebRtcVideoReceiveStream::GetVideoReceiverInfo(
         stats.rtx_rtp_stats->packet_counter.header_bytes +
         stats.rtx_rtp_stats->packet_counter.padding_bytes;
     info.packets_received += stats.rtx_rtp_stats->packet_counter.packets;
+  }
+
+  if (flexfec_stream_) {
+    const webrtc::ReceiveStatistics* fec_stats = flexfec_stream_->GetStats();
+    if (fec_stats) {
+      const webrtc::StreamStatistician* statistican =
+          fec_stats->GetStatistician(flexfec_config_.rtp.remote_ssrc);
+      if (statistican) {
+        const webrtc::RtpReceiveStats fec_rtp_stats = statistican->GetStats();
+        info.fec_packets_received = fec_rtp_stats.packet_counter.packets;
+        // TODO(bugs.webrtc.org/15250): implement fecPacketsDiscarded.
+        info.fec_bytes_received = fec_rtp_stats.packet_counter.payload_bytes;
+        // FEC information gets added to primary counters.
+        info.payload_bytes_received +=
+            fec_rtp_stats.packet_counter.payload_bytes;
+        info.header_and_padding_bytes_received +=
+            fec_rtp_stats.packet_counter.header_bytes +
+            fec_rtp_stats.packet_counter.padding_bytes;
+        info.packets_received += fec_rtp_stats.packet_counter.packets;
+      } else {
+        info.fec_packets_received = 0;
+      }
+    }
   }
 
   if (log_stats)
